36c4cbdf2d0eea2945bc63beb12380954f99023c
vivekshah02 <vivekshah45678@gmail.com>
2024-10-01 21:26:52 +0530

    new commit

--- Basics/22_loop.js
+++ Basics/22_loop.js
@@ -2,8 +2,8 @@ for(let i = 0; i < 5 ; i++){
     console.log(i);             //0 1 2 3 4
 }
 
-const a = 1
-const b = 2
+const a = 1;
+const b = 2;
 console.log(a + ' + ' + b + ' = ' + a+b);
 
 //break => breaks the loop
@@ -22,5 +22,5 @@ do{
     console.log(y);      // 1 2 3 4 5 6 7 8 9 10
     y++;
 
-}while( y <= 10)
+}while( y <= 10);
 
